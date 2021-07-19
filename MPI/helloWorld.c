97b8cc031051bc714d0bb9a521f11c388c8c0e69
Sobika Senthilnathan <76904383+Sobika2531@users.noreply.github.com>
2021-07-19 15:59:57 +0530

    Create helloWorld.c

---
+++ MPI/helloWorld.c
@@ -0,0 +1,21 @@
+#include <mpi.h>   
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) 
+{
+  int my_rank; 
+  int size;
+  
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
+  MPI_Comm_size(MPI_COMM_WORLD, &size); 
+  
+  printf("Hello world! I'm rank (processor number) %d of size %d\n", my_rank, size);
+  
+  if (my_rank == 0) 
+    printf("That is all for now!\n");
+  
+  MPI_Finalize();  
+  exit(0);
+}
