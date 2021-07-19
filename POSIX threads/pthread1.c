050d1068dbf43f22efb7466e099c3cfe8cf4c092
Sobika Senthilnathan <76904383+Sobika2531@users.noreply.github.com>
2021-07-19 14:50:43 +0530

    Create pthread1.c

---
+++ POSIX threads/pthread1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+
+void* func(void* p)
+{
+  long tid=(long)p;
+  printf("From the function, the thread id = %ld\n",tid); //get current thread id
+  pthread_exit(NULL);
+  return NULL;
+}
+
+int main() 
+{
+  int rc;
+  pthread_t thread; // declare thread
+
+  clock_t start=clock();
+
+  rc=pthread_create(&thread, NULL, func, NULL);
+
+  printf("From the main function, the thread id = %d\n",rc); pthread_join(thread, NULL); //join with main thread
+
+  clock_t end=clock(); printf("time is %f\n",(double)(end-start)/CLOCKS_PER_SEC);
+
+  return 0;
+}
